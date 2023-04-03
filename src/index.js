5607016eee0423b572d59da23d0830dae255b3a4
yumesou <yumesou123@gmail.com>
2023-04-03 18:20:09 -0300

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter as Router } from "react-router-dom";
 
 import './index.css';
 
@@ -8,5 +9,7 @@ import App from './App.jsx';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-    <App/>,
+    <Router>
+        <App/>
+    </Router>,
 )
