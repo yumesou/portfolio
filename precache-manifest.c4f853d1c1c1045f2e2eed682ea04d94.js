b367e4768fe93f48f35affa17b4676b2c5ba232f
yumesou <yumesou123@gmail.com>
2023-04-12 13:15:59 -0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.c4f853d1c1c1045f2e2eed682ea04d94.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "b20e61db25a6471cdc53",
+    "url": "/static/css/main.73d928dd.chunk.css"
+  },
+  {
+    "revision": "b20e61db25a6471cdc53",
+    "url": "/static/js/main.b20e61db.chunk.js"
+  },
+  {
+    "revision": "1af3950e2f8d41bb904e",
+    "url": "/static/css/1.6d3368a0.chunk.css"
+  },
+  {
+    "revision": "1af3950e2f8d41bb904e",
+    "url": "/static/js/1.1af3950e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "609832303f1528ece35754118839f12e",
+    "url": "/static/media/template.60983230.png"
+  },
+  {
+    "revision": "d5fc690da8c036aa8a59b44660acbba8",
+    "url": "/static/media/home.d5fc690d.svg"
+  },
+  {
+    "revision": "c5b215c11979f1638f3ce0a97e6f1403",
+    "url": "/static/media/skills.c5b215c1.svg"
+  },
+  {
+    "revision": "caa8b030e18aaeaf4d5ec19d4d9c3ee1",
+    "url": "/static/media/projects.caa8b030.svg"
+  },
+  {
+    "revision": "1bdfdf39ff8b51565eaa1371541f460a",
+    "url": "/static/media/contact.1bdfdf39.svg"
+  },
+  {
+    "revision": "c030a537d9cf966feee5a4d2b0b9a007",
+    "url": "/static/media/icon.c030a537.svg"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/static/media/peaple.a8822e20.svg"
+  },
+  {
+    "revision": "a793de690ce2327bd75c20f4b7ca7d0a",
+    "url": "/static/media/logo.a793de69.svg"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/static/media/background.5f3ca497.png"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "37b0b9cd99d7f80130a45ac23d9bcdf1",
+    "url": "/index.html"
+  }
+];
